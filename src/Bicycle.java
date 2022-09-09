93b000341d2edae782537897b2f8e5fd514d0931
IgorBerger03 <igor.berger98@yandex.ru>
2022-09-09 15:49:41 +0500

    HW 2.2.2

---
+++ src/Bicycle.java
@@ -0,0 +1,14 @@
+public class Bicycle extends Transport {
+
+    public Bicycle(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+
+    @Override
+    public void check(Transport transport) {
+        super.check(transport);
+    }
+}
